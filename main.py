355582345034aea81cd0fc9eec41cfaad1d2c59b
Ranim1997 <65516141+Ranim1997@users.noreply.github.com>
2020-12-14 16:32:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import jwt
 from flask import Flask, jsonify, request, abort
 
 
-JWT_SECRET = os.environ.get('JWT_SECRET', 'myjwtsecret')
-LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-
 
 def _logger():
     '''
